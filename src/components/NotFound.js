26e7e95b44e88100f87cb4ef7064efd604c8b13a
Joshua <olajidejoshua4real@gmail.com>
2019-09-19 12:26:26 +0100

    Form event handling added

---
+++ src/components/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFound = () =>(
+    <div>
+        <h2>Not Found, Please check your link again</h2>
+    </div>
+)
+
+export default NotFound
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -4,12 +4,23 @@ import { getFunName } from '../helpers';
 
 
 class StorePicker extends React.Component{
+
+	myInput = React.createRef();
+	goToStore = e => {
+		// 1. Prevent form from submitting
+		e.preventDefault();
+		//2. Get the text from the input
+		const storeName = myInput.current.value;
+		//3.  Change the page to whatever the user entered
+		this.history.push(`/store/${storeName}`);
+	}
 	render() {
 		return(
-			<form className="store-selector">	
+			<form className="store-selector" onSubmit={this.goToStore}>	
 				<h2> Please Enter A Store </h2>
 				<input 
 				type="text" 
+				ref={this.myInput}
 				required 
 				placeholder="Store Name" 
 				defaultValue={getFunName()}/>
