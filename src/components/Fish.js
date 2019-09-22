79cfa092e0bc630c7057fec30a665e6bdb41fb70
joshtom <olajidejoshua4real@gmail.com>
2019-09-22 17:20:23 -0400

    Update Add and Fish.js

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { formatPrice } from '../helpers'
 
 class Fish extends React.Component{
+    handleClick = () => {
+        this.props.addToOrder(this.props.index);
+    }
     render() {
         // Using es6 destructuring to set multiple variables 
         const { desc, image, name, price, status } = this.props.details;
-        const isAvailable = status == true
+        const isAvailable = status === "available"
         return(
            <li className="menu-fish">
                <img src={image} alt={image} />
@@ -13,7 +16,7 @@ class Fish extends React.Component{
                 <span className="price">{formatPrice(price)}</span>
             </h3>
             <p>{desc}</p>
-            <button disabled={!isAvailable}> {isAvailable ? 'Add to Order' : 'Sold Out'} </button>
+            <button disabled={!isAvailable} onClick={this.handleClick}> {isAvailable ? 'Add to Order' : 'Sold Out!'} </button>
 
            </li>
         )           
