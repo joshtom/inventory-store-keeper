97b4abfa2d27aaa5dbe07e97576d9c8b34c6f30a
joshtom <olajidejoshua4real@gmail.com>
2019-09-22 15:37:57 -0400

    Update Fish.js file

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import formatPrice from '../helpers'
+import { formatPrice } from '../helpers'
 
 class Fish extends React.Component{
     render() {
@@ -11,6 +11,8 @@ class Fish extends React.Component{
             <h3 className="fish-name">{name}
                 <span className="price">{formatPrice(price)}</span>
             </h3>
+            <p>{desc}</p>
+            <button>Add to Cart</button>
 
            </li>
         )           
